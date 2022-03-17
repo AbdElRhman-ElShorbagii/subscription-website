4a474fa6b61c33efbf740534ce835c00d140fee8
AbdELRhman-ElShorbagii <abdelrhmanelshorbagii@gmail.com>
2022-03-17 16:21:10 +0200

    fixing if condition

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,14 +32,6 @@
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
-                            @if(!Auth::guard('admin')->check() || !Auth::guard('user')->check())
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">Login</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">Register</a>
-                            </li>
-                            @endif
                             @if(Auth::guard('admin')->check())
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a>
@@ -47,10 +39,16 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('signout') }}">Logout</a>
                             </li>
-                            @endif
-                            @if(Auth::guard('user')->check())
+                            @elseif(Auth::guard('user')->check())
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('signout') }}">Logout</a>
+                            </li>
+                            @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">Register</a>
                             </li>
                             @endif
                         </ul>
